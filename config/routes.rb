da309e327a2bbf5b8f1a82be0016eaad15310a1b
corinnekunze <corinnekunze@gmail.com>
2013-12-19 22:08:38 -0800

    Adding comment model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 GuestBook::Application.routes.draw do
-  get "homepage/index"
+root 'comments#new'
+
+resources :comments
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
